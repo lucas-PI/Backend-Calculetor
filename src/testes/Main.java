04a2c549a1781f2381d5f66371b4417860f36377
lucas-PI <lucas22inacio@hotmail.com>
2024-07-25 12:35:16 -0300

    utilizacao do metodo onoroff

--- src/testes/Main.java
+++ src/testes/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         ControllerOperacao controller = new ControllerOperacao(calculadora);
-        controller.startCalculato();
+        controller.onOrOff();
     }
 }
