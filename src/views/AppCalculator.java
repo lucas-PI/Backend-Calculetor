a582c1fdc9af61c1fd19e33edd3b0340a4566366
lucas-PI <lucas22inacio@hotmail.com>
2024-07-30 14:54:49 -0300

    feat: tentando criar umas interface para a calculadora

--- src/views/AppCalculator.java
+++ src/views/AppCalculator.java
@@ -1,6 +1,8 @@
 package views;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AppCalculator extends JFrame {
 
@@ -15,14 +17,99 @@ public class AppCalculator extends JFrame {
     private JButton btnSeven;
     private JButton btnPoint;
     private JButton btnThree;
-    private JButton btnFIve;
-    private JButton btnEitgh;
+    private JButton btnFive;
+    private JButton btnEight;
     private JButton btnZero;
     private JButton plus;
     private JButton divide;
     private JButton minus;
     private JButton mutiply;
     private JButton equals;
+
+    public AppCalculator() {
+        btnOne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnOneText = textDisplay.getText() + btnOne.getText() ;
+                textDisplay.setText(btnOneText);
+            }
+        });
+        btnTwo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnTwoText = textDisplay.getText() + btnTwo.getText();
+                textDisplay.setText(btnTwoText);
+
+            }
+        });
+
+        btnThree.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnThreeText = textDisplay.getText() + btnThree.getText();
+                textDisplay.setText(btnThreeText);
+
+            }
+        });
+        btnFour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnFourText = textDisplay.getText() + btnFour.getText();
+                textDisplay.setText(btnFourText);
+
+            }
+        });
+
+        btnFive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnFiveText = textDisplay.getText() + btnFive.getText();
+                textDisplay.setText(btnFiveText);
+            }
+        });
+
+        btnSix.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnSixText = textDisplay.getText() + btnSix.getText();
+                textDisplay.setText(btnSixText);
+
+            }
+        });
+        btnSeven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnSevenText = textDisplay.getText() + btnSeven.getText();
+                textDisplay.setText(btnSevenText);
+
+            }
+        });
+        btnEight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnEightText = textDisplay.getText() + btnEight.getText();
+                textDisplay.setText(btnEightText);
+
+            }
+        });
+        btnNine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnNineText = textDisplay.getText() + btnNine.getText();
+                textDisplay.setText(btnNineText);
+
+            }
+        });
+        btnZero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnZeroText = textDisplay.getText() + btnZero.getText();
+                textDisplay.setText(btnZeroText);
+
+            }
+        });
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
